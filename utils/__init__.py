894df49d78ca66397e55f975179d48171ebc971f
zero91 <jianzhang9102@gmail.com>
2016-04-21 13:11:51 +0800

    add utils

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Useful utilities for daily use.
+===============================
+"""
+
+from . import check, shell
+
+__all__ = ['check', 'shell']
---
+++ utils/check.py
@@ -0,0 +1,29 @@
+"""Utility for doing small things quickly.
+"""
+
+# Author: Donald Cheung <jianzhang9102@gmail.com>
+
+import re
+
+def is_float(s):
+    """Return True if s is a float number, otherwise return False.
+
+    Parameters
+    ----------
+    s : string
+        The string to check
+
+    Returns
+    -------
+    is_float_num : boolean
+        True if s is a float number, otherwise False.
+
+    Examples
+    --------
+    >>> is_float('1.498e-12')
+    True
+
+    >>> is_float('1.498e-1.2')
+    True
+    """
+    return re.match(re.compile("^[-+]?[0-9]+(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?$"), s) is not None
