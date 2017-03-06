b965fb453333f063fa67836163d8b707d5472870
zhangjian09 <zhangjian09@baidu.com>
2017-03-07 02:39:26 +0800

    update depend_libs/libboost_python.so.1.61.0

--- platform/opt/__init__.py
+++ platform/opt/__init__.py
@@ -5,13 +5,15 @@ Tools for platform opt.
 
 def __load_librbary():
     import os
-    import glob
     import ctypes
-    map(ctypes.cdll.LoadLibrary,
-        glob.glob("{0}/depend_libs/*".format(os.path.dirname(os.path.realpath(__file__)))))
+
+    # must be loaded as the following order:
+    loading_list = ["libgcc_s.so.1", "libstdc++.so.6", "libboost_python.so.1.61.0"]
+    for lib in loading_list:
+        ctypes.cdll.LoadLibrary("{0}/depend_libs/{1}".format(
+                        os.path.dirname(os.path.realpath(__file__)), lib))
 
 __load_librbary()
-
 
 from . import configure
 from . import hadoop
