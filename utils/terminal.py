40221ba9878ab8713ad88df725a2bc3e188a1f7b
zero91 <jianzhang9102@gmail.com>
2018-07-03 00:17:22 -0700

    add terminal.py

--- utils/terminal.py
+++ utils/terminal.py
@@ -1,8 +1,8 @@
-"""Small functions for manipulating shell's display effect.
+"""Small functions for manipulating terminal's display effect.
 """
-
-# Author: Donald Cheung <jianzhang9102@gmail.com>
-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 import os
 import re
 import sys
@@ -12,7 +12,7 @@ def tint(s, font_color="red", bg_color=None, highlight=False):
 
     Parameters
     ----------
-    s : string
+    s : str
         The string to get a tint.
 
     font_color : string
@@ -29,8 +29,9 @@ def tint(s, font_color="red", bg_color=None, highlight=False):
 
     Returns
     -------
-    tint_str : string
-        String contains tint.
+    tint_str : str
+        Tinted string.
+
     """
     code_list = []
     if highlight:
@@ -68,26 +69,30 @@ def _tint_code(color, place):
     -------
     tint_code: integer
         Color's tint code.
+
     """
-    code_dict = dict()
-    code_dict["font"] = { "black"  : 30, 
-                          "red"    : 31,
-                          "green"  : 32,
-                          "yellow" : 33,
-                          "blue"   : 34,
-                          "purple" : 35,
-                          "cyan"   : 36,
-                          "white"  : 37 }
-
-    code_dict["bg"] = { "black"  : 40,
-                        "red"    : 41,
-                        "green"  : 42,
-                        "yellow" : 43,
-                        "blue"   : 44,
-                        "purple" : 45,
-                        "cyan"   : 46,
-                        "white"  : 47 }
-
+    code_dict = {
+        "font": {
+            "black"  : 30, 
+            "red"    : 31,
+            "green"  : 32,
+            "yellow" : 33,
+            "blue"   : 34,
+            "purple" : 35,
+            "cyan"   : 36,
+            "white"  : 37
+        },
+        "bg": {
+            "black"  : 40,
+            "red"    : 41,
+            "green"  : 42,
+            "yellow" : 43,
+            "blue"   : 44,
+            "purple" : 45,
+            "cyan"   : 46,
+            "white"  : 47
+        }
+    }
     if place not in code_dict:
         raise ValueError('Invalid place %s' % place)
 
