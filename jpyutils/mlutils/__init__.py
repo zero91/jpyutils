5c66e9b11e688be0fade90245dcfeef4dbb238ad
zero91 <jianzhang9102@gmail.com>
2019-02-28 01:20:02 +0800

    update mlutils.__init__.py for TensorFlow Import Error

--- jpyutils/mlutils/__init__.py
+++ jpyutils/mlutils/__init__.py
@@ -1,4 +1,10 @@
 from . import tags
-from . import text
 
-__all__ = ["tags", "text"]
+__all__ = ["tags"]
+
+try:
+    from . import text
+    __all__.append("text")
+except ImportError as e:
+    import logging
+    logging.warning("Load module 'text' failed, ignored it")
