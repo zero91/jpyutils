b7642d811e55f927478ed12c48ff0ed748a1cc64
zero91 <jianzhang9102@gmail.com>
2020-01-16 14:07:35 +0800

    recover utils/cjk function to manipulate chinese

--- lanfang/utils/__init__.py
+++ lanfang/utils/__init__.py
@@ -7,6 +7,7 @@ from lanfang.utils import terminal
 from lanfang.utils import disk
 from lanfang.utils import random
 from lanfang.utils import func
+from lanfang.utils import cjk
 
 
 __all__ = [_s for _s in dir() if not _s.startswith('_')]
